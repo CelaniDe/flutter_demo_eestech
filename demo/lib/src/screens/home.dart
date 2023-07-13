2eca584f646c1641e95ea09ac43aa0a295bd8943
lej0hn <johnydaras@gmail.com>
2023-07-13 19:25:11 +0300

    Added pou in home page

--- demo/lib/src/screens/home.dart
+++ demo/lib/src/screens/home.dart
@@ -11,7 +11,6 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
-
   final User? user = Auth().currentUSer;
 
   Future<void> signOut() async {
@@ -26,10 +25,18 @@ class _homeState extends State<home> {
     return Text(user?.email ?? 'User email');
   }
 
-  List<String> childrenData = ['Child 1', 'Child 2', 'Child 3', 'Child 1', 'Child 2', 'Child 3', 'Child 1', 'Child 2', 'Child 3'];
+  List<String> childrenData = [
+    'Child 1',
+    'Child 2',
+    'Child 3',
+    'Child 1',
+    'Child 2',
+    'Child 3',
+    'Child 1',
+    'Child 2',
+    'Child 3'
+  ];
   List<Widget> widgetList = [];
-
-
 
   @override
   void initState() {
@@ -49,61 +56,76 @@ class _homeState extends State<home> {
     }).toList();
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:
-      Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child:
-              Text(
-                'Welcome to the Park',
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Text(
+            'Welcome to the Park',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: widgetList,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Container(
+            width: 450,
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: Image.asset('assets/park.png').image,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        Padding(
+            padding: EdgeInsets.only(top: 20, left: 100),
+            child: Container(
+              color: Colors.lightBlue,
+              child: Text(
+                'Welcome! My name is Maou and \ntogether we will learn about food waste',
                 style: TextStyle(
-                  fontSize: 30,
+                  fontSize: 15,
                   fontWeight: FontWeight.bold,
                   color: Colors.black,
-                  
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child:
-              SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: widgetList,
-              ),
-            ),
-
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child:
-              Container(
-                width: 450,
-                height: 400,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: Image.asset('assets/park.png').image,
-                    fit: BoxFit.cover,
-                  ),
                 ),
               ),
+            )),
+        Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: Image.asset('assets/pou_img.png').image,
+                fit: BoxFit.cover,
+              ),
             ),
-            
-
-          ],
-
-        )
-    );
+          ),
+        ),
+      ],
+    ));
   }
 }
