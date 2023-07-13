b684673560fedf77a3f2f3e294187f4044d88094
Thanasis Karezos <thankarezos@gmail.com>
2023-07-13 15:16:45 +0300

    main page and coins

--- demo/lib/src/screens/home_page.dart
+++ demo/lib/src/screens/home_page.dart
@@ -3,6 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'dart:async';
+import 'package:demo/coinfield.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -13,6 +16,48 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final User? user = Auth().currentUSer;
+
+  int coinCount = 0;
+  final DatabaseReference coinsRef =
+    FirebaseDatabase.instance.reference().child('coins');
+
+
+
+
+Future<int> fetchUserCoins() async {
+  int userCoins = 0;
+
+  if (user != null) {
+    
+
+    DocumentSnapshot documentSnapshot =
+        await FirebaseFirestore.instance.collection('users').doc(user!.uid).get();
+    print(user!.uid);
+
+    if (documentSnapshot.exists) {
+      Map<String, dynamic>? data = documentSnapshot.data() as Map<String, dynamic>?;
+      if (data != null) {
+        userCoins = data['coins'] ?? 0;
+      }
+    }
+  }
+
+  return userCoins;
+}
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserCoins().then((userCoins) {
+      setState(() {
+        coinCount = userCoins;
+      });
+    });
+  }
+
+  Widget _coinCount() {
+    return Text('$coinCount');
+  }
 
   Future<void> signOut() async {
     await Auth().signOut();
@@ -34,7 +79,14 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: _title(),
+          title: Row(
+          children: <Widget>[
+            _title(),
+            Spacer(),
+            CoinField(coinImagePath: 'assets/coin.png', coinCount: coinCount),
+          ],
+        )
+
       ),
       body: Container(
         height: double.infinity,
@@ -43,7 +95,59 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[_userUid(), _signOutButton()],
+          children: <Widget>[_userUid(), _signOutButton(),
+            Container(
+              width: 450,
+              height: 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: Image.asset('assets/park.png').image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            
+
+          ],
+
+        ),
+      ),
+        bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              child: IconButton(
+                icon: Image.asset('assets/inventory.png'),
+                onPressed: () {
+                  // Handle home button press
+                },
+              ),
+            ),
+            Container(
+              width: 60,
+              height: 60,
+              child: IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  // Handle search button press
+                },
+              ),
+            ),
+            Container(
+              width: 60,
+              height: 60,
+              child: IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: () {
+                  // Handle settings button press
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
