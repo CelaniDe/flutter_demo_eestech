2cb70b65b986906cd8a1ca941c62edb25473e6f3
CelaniDe <celanide@gmail.com>
2023-07-13 19:17:12 +0300

    Add Inventory

--- demo/lib/src/screens/inventory.dart
+++ demo/lib/src/screens/inventory.dart
@@ -2,6 +2,14 @@ import 'package:demo/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+class Product {
+  String name;
+  DateTime expiryDate;
+  bool selected;
+
+  Product(this.name, this.expiryDate, {this.selected = false});
+}
+
 class inventory extends StatefulWidget {
   const inventory({Key? key}) : super(key: key);
 
@@ -10,8 +18,14 @@ class inventory extends StatefulWidget {
 }
 
 class _inventoryState extends State<inventory> {
-
   final User? user = Auth().currentUSer;
+
+  List<Product> productList = [
+    Product('Product 1', DateTime.now().add(Duration(days: 1))),
+    Product('Product 2', DateTime.now().add(Duration(days: 5))),
+    Product('Product 3', DateTime.now().add(Duration(days: 10))),
+    // Add more products here
+  ];
 
   Future<void> signOut() async {
     await Auth().signOut();
@@ -24,17 +38,29 @@ class _inventoryState extends State<inventory> {
   Widget _userUid() {
     return Text(user?.email ?? 'User email');
   }
-  
-    @override
-    Widget build(BuildContext context) {
-      return Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
 
-        )
-      );
-
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      child: ListView.builder(
+        itemCount: productList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            color: productList[index].selected ? Colors.green : null,
+            child: ListTile(
+              title: Text(productList[index].name),
+              subtitle: Text(
+                  "Expires on: ${productList[index].expiryDate.toLocal().toString().split(' ')[0]}"),
+              onTap: () {
+                setState(() {
+                  productList[index].selected = !productList[index].selected;
+                });
+              },
+            ),
+          );
+        },
+      ),
+    );
   }
+}
