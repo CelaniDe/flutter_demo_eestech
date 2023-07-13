ad62c5861befbb01dc7a979bc9592e15046fefbc
lej0hn <johnydaras@gmail.com>
2023-07-13 18:27:42 +0300

    Completed scanner functionality

--- demo/lib/src/screens/home.dart
+++ demo/lib/src/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:demo/auth.dart';
+import 'package:demo/src/screens/scanner.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -11,7 +12,6 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
-
   final User? user = Auth().currentUSer;
 
   Future<void> signOut() async {
@@ -26,10 +26,18 @@ class _homeState extends State<home> {
     return Text(user?.email ?? 'User email');
   }
 
-  List<String> childrenData = ['Child 1', 'Child 2', 'Child 3', 'Child 1', 'Child 2', 'Child 3', 'Child 1', 'Child 2', 'Child 3'];
+  List<String> childrenData = [
+    'Child 1',
+    'Child 2',
+    'Child 3',
+    'Child 1',
+    'Child 2',
+    'Child 3',
+    'Child 1',
+    'Child 2',
+    'Child 3'
+  ];
   List<Widget> widgetList = [];
-
-
 
   @override
   void initState() {
@@ -49,61 +57,58 @@ class _homeState extends State<home> {
     }).toList();
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:
-      Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child:
-              Text(
-                'Welcome to the Park',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  
-                ),
-                textAlign: TextAlign.left,
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Text(
+            'Welcome to the Park',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => Scanner()),
+            );
+          },
+          child: Text('Scan Barcode'),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: widgetList,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Container(
+            width: 450,
+            height: 400,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: Image.asset('assets/park.png').image,
+                fit: BoxFit.cover,
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child:
-              SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: widgetList,
-              ),
-            ),
-
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child:
-              Container(
-                width: 450,
-                height: 400,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: Image.asset('assets/park.png').image,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-            
-
-          ],
-
-        )
-    );
+          ),
+        ),
+      ],
+    ));
   }
 }
--- demo/lib/src/screens/scanner.dart
+++ demo/lib/src/screens/scanner.dart
@@ -1,25 +1,49 @@
+import 'package:flutter/material.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
 
-Future<void> scanBarcode() async {
-  try {
-    String barcode = await BarcodeScanner.scan(); // Scan the barcode
+class Scanner extends StatefulWidget {
+  @override
+  _ScannerState createState() => _ScannerState();
+}
 
-    // Do something with the barcode value (e.g., display it)
-    print('Scanned barcode: $barcode');
-  } on PlatformException catch (e) {
-    if (e.code == BarcodeScanner.CameraAccessDenied) {
-      // Handle camera access denied error
-      print('Camera permission denied');
-    } else {
-      // Handle other exceptions
+class _ScannerState extends State<Scanner> {
+  Future<void> scanBarcode() async {
+    try {
+      String barcode = await BarcodeScanner.scan();
+
+      // Do something with the scanned barcode value
+      print('Scanned barcode: $barcode');
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        print('Camera permission denied');
+      } else {
+        print('Error: $e');
+      }
+    } on FormatException {
+      print('Scan cancelled');
+    } catch (e) {
       print('Error: $e');
     }
-  } on FormatException {
-    // User pressed the back button or cancelled the scan
-    print('Scan cancelled');
-  } catch (e) {
-    // Handle other exceptions
-    print('Error: $e');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Barcode Scanner'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: scanBarcode,
+              child: Text('Scan Barcode'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
