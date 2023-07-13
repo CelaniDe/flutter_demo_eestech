44059d82371256630e628a6babc2b2271ca326c4
Thanasis Karezos <thankarezos@gmail.com>
2023-07-13 23:03:14 +0300

    add item

--- demo/lib/src/screens/inventory.dart
+++ demo/lib/src/screens/inventory.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:demo/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,38 @@ class _inventoryState extends State<inventory> {
     await Auth().signOut();
   }
 
+  Future<void> addItem() async {
+    final user = FirebaseAuth.instance.currentUser;
+    final userCollection = FirebaseFirestore.instance.collection('users');
+
+    if (user != null) {
+
+      final userDoc = userCollection.doc(user.uid);
+      final productCollection = FirebaseFirestore.instance.collection('products');
+      final productDoc = productCollection.doc('555555555');
+
+      final productSnapshot = await productDoc.get();
+
+      if (productSnapshot.exists) {
+        final productData = productSnapshot.data();
+
+        // Add the product data to the user's product list
+        final products = await userDoc.collection('products').get();
+        await userDoc.collection('products').add(productData!);
+        print(products.docs.map((e) => e.data()));
+      }
+    }
+
+  Widget _addItem() {
+
+      return 
+        ElevatedButton(
+          onPressed: addItem,
+          child: Text('Scan Barcode'),
+        );
+    }
+  }
+
   Widget _signOutButton() {
     return ElevatedButton(onPressed: signOut, child: const Text('Sign out'));
   }
@@ -31,7 +64,11 @@ class _inventoryState extends State<inventory> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+        children: <Widget>[            
+            ElevatedButton(
+              onPressed: addItem,
+              child: Text('add item'),
+            ),],
 
         )
       );
